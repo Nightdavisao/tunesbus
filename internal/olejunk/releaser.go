15f2e4616085f3dd1e70889c9734e30394d16a50
Nightdavisao <nightdavisao27@protonmail.com>
2026-07-21 17:10:16 -0300

    use a "set" (map) instead of list

--- internal/olejunk/releaser.go
+++ internal/olejunk/releaser.go
@@ -2,41 +2,48 @@ package olejunk
 
 import (
 	"sync"
+
 	"github.com/charmbracelet/log"
 	"github.com/go-ole/go-ole"
 )
 
 type OleReleaser struct {
 	mu      sync.RWMutex
-	objects []*ole.IUnknown
+	objects map[*ole.IUnknown]struct{}
 }
 
 func NewOleReleaser() *OleReleaser {
 	return &OleReleaser{
-		objects: make([]*ole.IUnknown, 0),
+		objects: make(map[*ole.IUnknown]struct{}),
 	}
 }
 
 func (r *OleReleaser) Add(obj *ole.IUnknown) {
 	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	log.Debug("OleReleaser: adding object", "object", obj)
-	if obj != nil {
-		obj.AddRef()
-		r.objects = append(r.objects, obj)
-	} // todo: warn if nil
-	r.mu.Unlock()
+	if obj == nil {
+		log.Warn("OleReleaser: attempted to add nil object")
+		return
+	}
+	if _, exists := r.objects[obj]; exists {
+		log.Debug("OleReleaser: object already tracked, skipping AddRef", "object", obj)
+		return
+	}
+	obj.AddRef()
+	r.objects[obj] = struct{}{}
 }
 
 func (r *OleReleaser) Release() {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	log.Debug("OleReleaser: releasing all objects", "length", len(r.objects))
-	for _, obj := range r.objects {
+	for obj := range r.objects {
 		log.Debug("OleReleaser: object to be released", "object", obj)
-		if obj != nil {
-			refCount := obj.Release()
-			log.Debug("OleReleaser: released object", "ref_count", refCount)
-		}
+		refCount := obj.Release()
+		log.Debug("OleReleaser: released object", "ref_count", refCount)
 	}
-	r.objects = nil
+	r.objects = make(map[*ole.IUnknown]struct{})
 }
